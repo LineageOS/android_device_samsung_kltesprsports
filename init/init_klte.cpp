dfa944eabff71effc464af6d91f3df839e63817d
Bruno Martins <bgcngm@gmail.com>
2018-04-01 10:58:29 +0100

    kltesprsports: libinit: Also override certain vendor build properties
    Change-Id: I2acce5df07619b4a737994b7d223f77b09425cb5

--- init/init_klte.cpp
+++ init/init_klte.cpp
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2013, The Linux Foundation. All rights reserved.
+   Copyright (c) 2018, The LineageOS Project. All rights reserved.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -66,10 +67,10 @@ void init_target_properties()
 
     if (bootloader.find("G860P") == 0) {
         /* kltesprsports */
-        property_override("ro.build.fingerprint", "samsung/kltesprsports/kltesprsports:6.0.1/MMB29M/G860PVPU2CQB2:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/kltesprsports/kltesprsports:6.0.1/MMB29M/G860PVPU2CQB2:user/release-keys");
         property_override("ro.build.description", "kltesprsports-user 6.0.1 MMB29M G860PVPU2CQB2 release-keys");
-        property_override("ro.product.model", "SM-G860P");
-        property_override("ro.product.device", "kltesprsports");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-G860P");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "kltesprsports");
         property_set("telephony.sms.pseudo_multipart", "1");
         cdma_properties("Sprint", "310120", "8");
     }
